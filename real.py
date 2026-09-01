485927f51190287b39779e4e5010e1edea696f0c
shruti <shrutivaidya00@gmail.com>
2026-09-01 14:52:12 +0530

    one line change

--- real.py
+++ real.py
@@ -5,6 +5,8 @@ print("\nStudent Information")
 print("Name:", name)
 print("Age:", age)
 print ("Welcome to the online checking device")
+
+print ("Display only in my branchgit ")
 if age >= 18:
     print("Status: Adult")
 else:
